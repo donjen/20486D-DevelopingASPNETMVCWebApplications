fd44d9f5b5c80891ee6911e42ba7442efaf1933b
Donnie Jensen <donjen@saif.com>
2020-01-29 14:44:43 -0800

    Completed Exercise 2

--- Allfiles/Mod07/Labfiles/01_Cupcakes_begin/Cupcakes/Startup.cs
+++ Allfiles/Mod07/Labfiles/01_Cupcakes_begin/Cupcakes/Startup.cs
@@ -2,22 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Cupcakes.Data;
+using Cupcakes.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Cupcakes
 {
     public class Startup
     {
+        private IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<ICupcakeRepository, CupcakeRepository>();
+      string connectionString = "Server=(localdb)\\MSSQLLocalDB;Database=BakeriesDb;Trusted_Connection=True;MultipleActiveResultSets=true";
+      services.AddDbContext<CupcakeContext>(options =>
+                 options.UseSqlServer(connectionString));
             services.AddMvc();
         }
 
-        public void Configure(IApplicationBuilder app)
+        public void Configure(IApplicationBuilder app, CupcakeContext cupcakeContext)
         {
+            cupcakeContext.Database.EnsureDeleted();
+            cupcakeContext.Database.EnsureCreated();
+
             app.UseStaticFiles();
 
             app.UseMvc(routes =>
